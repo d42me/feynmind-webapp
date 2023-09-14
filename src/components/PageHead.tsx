3ef0c68a82d4f63a9b9e6e3aecefc7521d5a8bf4
Dominik <dominik.scherm@reaction.link>
2023-09-14 14:12:48 -0700

    Add twitter og tags.

--- src/components/PageHead.tsx
+++ src/components/PageHead.tsx
@@ -21,6 +21,20 @@ export default function PageHead({ title }: PageHeadProps) {
         content="FeynMind is a finetuned GPT model based on the Feynman lectures. Dive deep into the enthralling world of physics and have conversations with Richard Feynman himself in a profound Caltech surrounding, where curiosity meets wisdom and the spirit of exploration is ever-present."
       />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image:alt" content="FeynMind Banner" />
+      <meta
+        name="twitter:title"
+        content="Philippine Standard Geographic Code API"
+      />
+      <meta
+        name="twitter:description"
+        content="FeynMind is a finetuned GPT model based on the Feynman lectures. Dive deep into the enthralling world of physics and have conversations with Richard Feynman himself in a profound Caltech surrounding, where curiosity meets wisdom and the spirit of exploration is ever-present."
+      />
+      <meta
+        name="twitter:image"
+        content="https://www.feynmind.ai/feynman-logo.jpg"
+      />
     </Head>
   );
 }
