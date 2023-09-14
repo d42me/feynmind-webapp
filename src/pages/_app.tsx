99ffba54cd64579c0dd5fff9c2f064d2ca0e6661
Dominik <dominik.scherm@reaction.link>
2023-09-14 14:57:16 -0700

    Add next plausible.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import PlausibleProvider from "next-plausible";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlausibleProvider domain="feynmind.ai">
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
